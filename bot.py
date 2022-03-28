022d710c389e69cbdb310a6ee743bb62ef957d6d
Davron Ergashev <davronee@gmail.com>
2022-03-28 17:15:00 +0500

    Initial Commit

--- bot.py
+++ bot.py
@@ -1,48 +1,79 @@
-import logging
-from typing import Any
+import telebot
+import bs4
+from Task import Task
+import parser
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
+#main variables
+TOKEN = '5009512905:AAHWIN7kPEdA3Br2Q0W3SZRujEZ3GOQFyx4'
+bot = telebot.TeleBot(TOKEN)
+task = Task()
 
-TOKEN = "5009512905:AAHWIN7kPEdA3Br2Q0W3SZRujEZ3GOQFyx4"
-dp = Dispatcher()
+#handlers
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    if not task.isRunning:
+        chat_id = message.chat.id
+        msg = bot.send_message(chat_id, 'Откуда парсить?')
+        bot.register_next_step_handler(msg, askSource)
+        task.isRunning = True
 
-logger = logging.getLogger(__name__)
+def askSource(message):
+    chat_id = message.chat.id
+    text = message.text.lower()
+    if text in task.names[0]:
+        task.mySource = 'top'
+        msg = bot.send_message(chat_id, 'За какой временной промежуток?')
+        bot.register_next_step_handler(msg, askAge)
+    elif text in task.names[1]:
+        task.mySource = 'all'
+        msg = bot.send_message(chat_id, 'Какой минимальный порог рейтинга?')
+        bot.register_next_step_handler(msg, askRating)
+    else:
+        msg = bot.send_message(chat_id, 'Такого раздела нет. Введите раздел корректно.')
+        bot.register_next_step_handler(msg, askSource)
+        return
 
+def askAge(message):
+    chat_id = message.chat.id
+    text = message.text.lower()
+    filters = task.filters[0]
+    if text not in filters:
+        msg = bot.send_message(chat_id, 'Такого временного промежутка нет. Введите порог корректно.')
+        bot.register_next_step_handler(msg, askAge)
+        return
+    task.myFilter = task.filters_code_names[0][filters.index(text)]
+    msg = bot.send_message(chat_id, 'Сколько страниц парсить?')
+    bot.register_next_step_handler(msg, askAmount)
 
-@dp.message(commands=["start"])
-async def command_start_handler(message: Message) -> None:
-    """
-    This handler receive messages with `/start` command
-    """
-    # Most of event objects has an aliases for API methods to be called in event context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage` method automatically
-    # or call API method directly via Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, <b>{message.from_user.full_name}!</b>")
+def askRating(message):
+    chat_id = message.chat.id
+    text = message.text.lower()
+    filters = task.filters[1]
+    if text not in filters:
+        msg = bot.send_message(chat_id, 'Такого порога нет. Введите порог корректно.')
+        bot.register_next_step_handler(msg, askRating)
+        return
+    task.myFilter = task.filters_code_names[1][filters.index(text)]
+    msg = bot.send_message(chat_id, 'Сколько страниц парсить?')
+    bot.register_next_step_handler(msg, askAmount)
 
+def askAmount(message):
+    chat_id = message.chat.id
+    text = message.text.lower()
+    if not text.isdigit():
+        msg = bot.send_message(chat_id, 'Количество страниц должно быть числом. Введите корректно.')
+        bot.register_next_step_handler(msg, askAmount)
+        return
+    if int(text) < 1 or int(text) > 11:
+        msg = bot.send_message(chat_id, 'Количество страниц должно быть >0 и <11. Введите корректно.')
+        bot.register_next_step_handler(msg, askAmount)
+        return
+    task.isRunning = False
+    output = ''
+    if task.mySource == 'top':
+        output = parser.getTitlesFromTop(int(text), task.myFilter)
+    else:
+        output = parser.getTitlesFromAll(int(text), task.myFilter)
+    msg = bot.send_message(chat_id, output)
 
-@dp.message()
-async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward received message back to the sender
-
-    By default message handler will handle all message types (like text, photo, sticker and etc.)
-    """
-    try:
-        # Send copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
-
-
-def main() -> None:
-    # Initialize Bot instance with an default parse mode which will be passed to all API calls
-    bot = Bot(TOKEN, parse_mode="HTML")
-    # And the run events dispatching
-    dp.run_polling(bot)
-
-
-if __name__ == "__main__":
-    main()
+bot.polling(none_stop=True)
